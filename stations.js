4f0515199d24c21eabfe11ddb70a1962babf34aa
ritiksinha <ritikk910@gmail.com>
2023-01-29 17:55:55 +0530

    experiment

--- stations.js
+++ stations.js
@@ -1,1 +1,8 @@
-console.log(stationObJ["welcome"]);
+const axios = require("axios");
+const getResponse = async (from, to) => {
+  const res = await axios.get(
+    `https://us-central1-delhimetroapi.cloudfunctions.net/route-get?from=${from}&to=${to}`
+  );
+  return res.data;
+};
+getResponse("Sarai", "Badarpur").then((data) => console.log(data));
